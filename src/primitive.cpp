e9670aca841b29fbb47163709ab56d380ed1586a
Glomzzz <glom@skillw.com>
2025-09-29 12:56:51 +0800

    refactor(REPL)

--- src/primitive.cpp
+++ src/primitive.cpp
@@ -36,15 +36,10 @@ void Context::add_primitive(const string& name, PrimitiveProc proc)
 void add_number_operations(Context& builder)
 {
     builder.add_primitive("+", primitives::add);
-
     builder.add_primitive("*", primitives::mul);
-
     builder.add_primitive("-", primitives::sub);
-
     builder.add_primitive("/", primitives::div);
-
     builder.add_primitive("remainder", primitives::remainder);
-
     builder.add_primitive("expt", primitives::power);
 }
 
